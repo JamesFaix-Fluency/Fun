00233f8d2d5539c389c913f3b6f9f11e48fa8a75
James Faix <jamesthefaix@gmail.com>
2017-10-08 01:41:24 -0400

    fixed LINQ methods

--- Fun.Linq/OptExtensions.cs
+++ Fun.Linq/OptExtensions.cs
@@ -8,10 +8,28 @@ namespace Fun.Linq
             this Opt<T1> @this,
             Func<T1, T2> projection) =>
             @this.OptMap(projection);
-
-        public static Opt<T2> SelectMany<T1, T2>(
+        
+        public static Opt<T3> SelectMany<T1, T2, T3>(
             this Opt<T1> @this,
-            Func<T1, Opt<T2>> projection) =>
-            @this.OptMap(projection);
+            Func<T1, Opt<T2>> projection,
+            Func<T1, T2, T3> resultSelector) =>
+            @this.OptMap(t1 => 
+                projection(t1).OptMap(t2 => 
+                    resultSelector(t1, t2)));
+
+
+        private static void Test()
+        {
+            var a = from x in Opt.Some(4)
+                    select x;
+            
+            var b = from x in Opt.Some(5)
+                    from y in Opt.Some(7)
+                    select x * y;
+
+            var c = from x in Opt.Some(5)
+                    from y in Opt.None<int>()
+                    select x + y;
+        }
     }
 }
--- Fun.Linq/TryExtensions.cs
+++ Fun.Linq/TryExtensions.cs
@@ -9,9 +9,26 @@ namespace Fun.Linq
             Func<T1, T2> projection) =>
             @this.TryMap(projection);
 
-        public static Try<T2> SelectMany<T1, T2>(
+        public static Try<T3> SelectMany<T1, T2, T3>(
             this Try<T1> @this,
-            Func<T1, Try<T2>> projection) =>
-            @this.TryMap(projection);
+            Func<T1, Try<T2>> projection,
+            Func<T1, T2, T3> resultSelector) =>
+            @this.TryMap(t1 =>
+                projection(t1).TryMap(t2 =>
+                    resultSelector(t1, t2)));
+        
+        private static void Test()
+        {
+            var a = from x in Try.Some(4)
+                    select x;
+
+            var b = from x in Try.Some(5)
+                    from y in Try.Some(7)
+                    select x * y;
+
+            var c = from x in Try.Some(5)
+                    from y in Try.Error<int>(new Exception())
+                    select x + y;
+        }
     }
 }
