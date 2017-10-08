f1dc9cb8e728026870bcc906fa9dc58352632948
James Faix <jamesthefaix@gmail.com>
2017-10-08 09:46:46 -0400

    Task linq extensions

---
+++ Fun.Linq/TaskExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Fun.Linq
+{
+    public static class TaskExtensions
+    {
+        public static async Task<T2> Select<T1, T2>(
+            this Task<T1> @this,
+            Func<T1, T2> projection)
+        {
+            return projection(await @this);
+        }
+
+        public static async Task<T3> SelectMany<T1, T2, T3>(
+            this Task<T1> @this,
+            Func<T1, Task<T2>> projection,
+            Func<T1, T2, T3> resultSelector)
+        {
+            var t1 = await @this;
+            var t2 = await projection(t1);
+            return resultSelector(t1, t2);
+        }
+    }
+}
---
+++ Fun.Tests/Linq/TaskQueryExpressionTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+using Fun.Linq;
+
+namespace Fun.Tests.Linq
+{
+    [TestFixture]
+    public class TaskQueryExpressionTests
+    {
+        private static Task<int> GetLongRunningTask(int n)
+        {
+            return Task.Run(() =>
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                return n;
+            });
+        }
+
+        #region Select
+
+        [Test]
+        public async Task FromSelect_ShouldMapResult()
+        {
+            var result = await
+                         from x in GetLongRunningTask(1)
+                         select x.ToString();
+
+            result.ShouldBe("1");
+        }
+
+        [Test]
+        public void FromSelect_ShouldNotBlock()
+        {
+            Assert.Fail();
+        }
+
+        #endregion
+
+        #region SelectMany
+
+        [Test]
+        public async Task FromFromSelect_ShouldMapResults()
+        {
+            var result = await
+                         from x in GetLongRunningTask(1)
+                         from y in GetLongRunningTask(2)
+                         select x + y;
+
+            result.ShouldBe(3);
+        }
+
+        #endregion
+    }
+}
