11681776a21c4c99cf732f7c90804dfb7dc3c63b
James Faix <jamesthefaix@gmail.com>
2017-10-09 09:36:27 -0400

    Moved files

---
+++ Fun/Modules/Disp.Module.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fun
+{
+    public static class Disp
+    {
+        public static Disp<T, TDisposable> Create<T, TDisposable>(T value, TDisposable resource)
+            where TDisposable : IDisposable
+        {
+            return new Disp<T, TDisposable>(value, resource);
+        }
+    }
+}
